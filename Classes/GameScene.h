0de0928d52ae77b9a68d37dc206ec077d31b28a1
1952888 <57441321+1952888@users.noreply.github.com>
2020-05-23 14:20:26 +0800

    死地图问题和游戏步数限制

--- Classes/GameScene.h
+++ Classes/GameScene.h
@@ -25,8 +25,7 @@ public:
 	Point getposition( int row , int col );
 
 	Point positionOfItem(int row, int col);
-	// 根据触摸的点位置，返回是地图中哪个精灵
-	SpriteShape* spriteOfPoint(Point *point);
+	
 
 	//用于更新每一帧的动作
 	void update(float t);
@@ -38,17 +37,6 @@ public:
 	void getColChain(SpriteShape* spr, std::list<SpriteShape*>& chainList);
 	void getRowChain(SpriteShape* spr, std::list<SpriteShape*>& chainList);
 	void fillSprite();
-	//移动动作
-	//关于鼠标触摸
-	/***** 触摸事件 *****/
-	bool onTouchBegan(Touch *touch, Event *unused);
-	void onTouchMoved(Touch *touch, Event *unused);
-	// 交换精灵
-	void swapSprite();
-	//剩余次数
-	void myFrequency();
-	//检查是否是死地图
-	bool checkIfDeadMap();
 
 	CREATE_FUNC(GameScene);
 private:
@@ -57,13 +45,8 @@ private:
 	// 二维数组
 	SpriteShape* map[ROWS][COLS];
 	float mapLBX, mapLBY;
-	//操纵的精灵
-	SpriteShape* startSprite;
-	SpriteShape* endSprite;
-	bool isAction, isFillSprite;//用来判断是否在执行动作，是否需要填补空缺
-	bool isTouchEna;
 
-	int m_frequency;
+	bool isAction, isFillSprite;//用来判断是否在执行动作，是否需要填补空缺
 };
 
 #endif // __GAME_SCENE_H__
